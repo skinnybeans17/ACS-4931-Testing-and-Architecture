6e084484ef3b849f114c9cbcd608defcc29daa77
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2023-02-09 09:18:47 -0800

    shapes

--- exercises/other-refactor-techniques/draw_shapes.py
+++ exercises/other-refactor-techniques/draw_shapes.py
@@ -1,42 +1,39 @@
 # by Kami Bigdely
 # Extract superclass.
-class Circle:
+class Shape:
+    def __init__(self, visible) -> None:
+        self.visible = visible
+
+    def set_visible(self,is_visible):
+        self.visible = is_visible
+
+class Circle(Shape):
     
     def __init__(self, x, y, r, visible = True):
+      super().__init__(visible)
       self.center_x = x
       self.center_y = y
       self.r = r
-      self.visible = visible
       
     def display(self):
         print('drew the circle.')
-        
-    def set_visible(self,is_visible):
-        self.visible = is_visible
         
     def get_center(self):
         return self.center_x, self.center_y
     
     
-class Rectangle:
+class Rectangle(Shape):
     
     def __init__(self, x, y, width, height, visible = True):
-        # left-bottom corner.
+        super().__init__(visible)
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-        self.visible = visible
         
     def display(self):
         if self.visible:
-            print('drew the rectable.')
-            
-    def hide(self):
-        self.visible = False
-        
-    def make_visible(self):
-        self.visible = True
+            print('drew the rectangle.')
         
     def get_center(self):
         return self.x + self.width/2, \
@@ -51,8 +48,5 @@ if __name__ == "__main__":
     print('center point',circle.get_center())
 
     rect = Rectangle(10, 10, 20, 5)
-    rect.hide()
     rect.display() # does not display because it's hidden.
-    rect.make_visible()
-    rect.display()
     print('center point',rect.get_center())
